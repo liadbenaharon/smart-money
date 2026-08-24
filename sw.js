ec183f8f068e7563bce7763c99ed74a7f58fbe79
liadbenaharon <liadbenaharon@gmail.com>
2026-08-24 16:32:20 +0300

    Bump PWA cache for savings update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smart-money-v4';
+const CACHE='smart-money-v5';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});

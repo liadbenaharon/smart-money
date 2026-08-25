ea567fa2ee53005334a4fb9637a05ba46a240fab
liadbenaharon <liadbenaharon@gmail.com>
2026-08-25 19:10:59 +0300

    Refresh cache after version badge fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smart-money-v13';
+const CACHE='smart-money-v14';
 const ASSETS=['./','./index.html','./style.css','./app.js','./recurring-fix.js','./v030-features.js','./manifest.json','./version.json','./CHANGELOG.md','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});

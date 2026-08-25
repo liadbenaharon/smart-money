1526b0421faaa1f5c9d50c7b603609992358866f
liadbenaharon <liadbenaharon@gmail.com>
2026-08-25 18:00:19 +0300

    Refresh PWA cache for polished UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='smart-money-v7';
+const CACHE='smart-money-v8';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
